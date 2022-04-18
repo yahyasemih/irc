66e593be098af15ab95e594e7d96d82ee0c3047f
Yahya Ez-zainabi <yez-zain@student.1337.ma>
2022-04-18 22:43:09 +0000

    Add base support of server config

---
+++ server_config.hpp
@@ -0,0 +1,36 @@
+#ifndef SERVER_CONFIG
+#define SERVER_CONFIG
+
+#include <string>
+#include <unordered_map>
+
+class server_config {
+public:
+	typedef std::unordered_map<std::string, std::string> operator_map;
+private:
+	std::string server_name;
+	std::string server_info;
+	operator_map operators;
+public:
+	server_config();
+	server_config(const std::string &config_file);
+	~server_config();
+
+	const operator_map &get_operators() const;
+};
+
+server_config::server_config() : server_name(":irc.1337.ma"), server_info("This an IRC server made in 1337 school") {
+}
+
+server_config::server_config(const std::string &config_file) {
+	//TODO: parse config file
+}
+
+server_config::~server_config() {
+}
+
+const server_config::operator_map &server_config::get_operators() const {
+	return operators;
+}
+
+#endif
