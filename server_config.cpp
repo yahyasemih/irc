30c9b156236418605abf2068c20565468b08e0c6
Zakariaa Oulhafiane <zakariaa.oulhafiane@gmail.com>
2022-04-24 18:24:30 +0000

    Parsing the config into an std::map finished

--- server_config.cpp
+++ server_config.cpp
@@ -5,6 +5,7 @@ server_config::server_config() : server_name("irc.1337.ma"),
 		version("leet-irc 1.0.0"),
 		user_modes("aioOrsw"),
 		channel_modes("ovimntklbeI") {
+			parse_conf();
 }
 
 server_config::server_config(const std::string &config_file) {
@@ -38,4 +39,45 @@ const std::string &server_config::get_user_modes() const {
 
 const std::string &server_config::get_channel_modes() const {
 	return channel_modes;
+}
+
+void			   server_config::parse_conf() {
+	std::ifstream inFile;
+	inFile.open("irc.conf");
+
+	std::stringstream strStream;
+	strStream << inFile.rdbuf();
+	std::string str = strStream.str();
+
+	std::regex re("(#.*[\\s]*$)");
+	str = std::regex_replace(str, re, "");
+
+	strStream.str(str);
+	std::string line;
+	re = "^\\[([A-Za-z]+)\\]$";
+	std::regex re2("^\\s*([^\\s;]\\S+)\\s*=\\s*([^#]*)$");
+	std::string key = "";
+	while (std::getline(strStream, line)) {
+		if (std::regex_match(line, re)) {
+			std::map<std::string, std::string> m;
+			std::smatch match;
+			std::regex_search(line, match, re);
+			key = match[1];
+			config[key] = m;
+		} else if (std::regex_match(line, re2)) {
+			std::smatch m;
+			std::regex_search(line, m, re2);
+			config[key][m[1]] = m[2];
+		}
+	}
+
+    //this for loop just temporarely to see results
+	for (std::map<std::string, std::map<std::string, std::string> >::iterator it= config.begin(); it!= config.end(); ++it) {
+		if (it->second.empty())
+			continue;
+		std::cout << "==========" << it->first << "==========" << std::endl;
+		for (std::map<std::string, std::string>::iterator it2 = it->second.begin(); it2 != it->second.end(); ++it2) {
+			std::cout << it2->first << " => " << it2->second << std::endl;
+		}
+	}
 }
