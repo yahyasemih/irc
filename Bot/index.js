ab4353fc39c9637554c6c13390b02bf92fba1c6f
Lahcen Saidi <lahcen.saidi88@gmail.com>
2022-04-28 17:56:26 +0000

    - trim the command and args if exist - check if the message has always less then 2 args to check the command provide from the box - change the catch client.say message in utils (is_available_cmd)

--- Bot/index.js
+++ Bot/index.js
@@ -1,8 +1,8 @@
 var irc = require("irc");
 const utils = require("./utils");
-const HOST = process.env.HOST ? process.env.HOST : "localhost"
-const PASSWORD = process.env.PASSWORD
-const PORT = process.env.PORT ? process.env.PORT : 6667
+const HOST = process.env.HOST ? process.env.HOST : "localhost";
+const PASSWORD = process.env.PASSWORD;
+const PORT = process.env.PORT ? process.env.PORT : 6667;
 
 var client = new irc.Client(HOST, "bot", {
   password: PASSWORD,
@@ -10,12 +10,12 @@ var client = new irc.Client(HOST, "bot", {
   userName: "Bot",
   realName: "1337 Bot",
   encoding: "UTF-8",
-  messageSplit:100,
+  messageSplit: 100,
   floodProtection: true,
   floodProtectionDelay: 1,
 });
 
-const channels = new Set([])
+const channels = new Set([]);
 
 const commands = {
   "!part": utils.part_cmd,
@@ -28,7 +28,7 @@ const commandsWithParams = {
   "!ft_is_available": utils.is_available_cmd,
   "!ft_points": utils.ft_points_cmd,
   "!ft_wallet": utils.ft_wallet_cmd,
-  "!ft_cursus_info": utils.ft_cursus_info_cmd
+  "!ft_cursus_info": utils.ft_cursus_info_cmd,
 };
 
 client.addListener("invite", function (channel, from, message) {
@@ -45,19 +45,37 @@ client.addListener("invite", function (channel, from, message) {
       client.say(channel, `3lach jriti 3lia a si ${by} :(`);
     });
 
-    client.addListener(`message${channel}`, async function (from, message) {
-      const args = message.split(" ");
+    client.addListener(`message${channel}`, async (from, message) => {
+      if (message) {
+        const string = message.trim().replace(/\s+/g, " "); //replace all whitespace with one space
+        const args = string.split(" "); //trim the message line
+        const args_len = args.length;
 
-      if (commands[message]) {
-        await commands[message](client, channel);
-      } else if (commandsWithParams[args[0]]) {
-        await commandsWithParams[args[0]](client, channel, args[1], args[0]);
+        /*
+          in just if args equaol to 1 or 2 and dont care about the rest
+          - if the message hase params go to --> commandsWithParams[command]
+          - else go to --> commands[command]
+        */
+        if (args_len <= 2) {
+          if (args_len == 1 && commands[args[0]]) {
+            await commands[args[0]](client, channel);
+          } else if (args_len == 2 && commandsWithParams[args[0]]) {
+            await commandsWithParams[args[0]](
+              client,
+              channel,
+              args[1],
+              args[0]
+            );
+          }
+        }
       }
     });
   }
   channels.add(channel);
 });
 
-client.addListener("error", function(message) {
-  console.log("Got error from server (" + message.rawCommand + "): " + message.command);
+client.addListener("error", function (message) {
+  console.log(
+    "Got error from server (" + message.rawCommand + "): " + message.command
+  );
 });
